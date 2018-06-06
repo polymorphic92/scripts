c03ffefdeb9b22f6fe88f8c587ec4385b28ad362
kylebrah <kylebrah@buffalo.edu>
2018-06-06 13:32:22 -0400

    go installer script

---
+++ golang-installer.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+VERSION='1.10.2'
+
+cd /home/$USER/Downloads && curl -O https://dl.google.com/go/go$VERSION.linux-amd64.tar.gz  #go.#.##.# needs to be a parmameter
+sudo tar -C /usr/local -xzf go$VERSION.linux-amd64.tar.gz
+# sudo rm -f /usr/local/go$VERSION.linux-amd64.tar.gz
+sudo rm -f /home/$USER/Downloads/go$VERSION.linux-amd64.tar.gz
+
+
+if ! grep -q 'export PATH=$PATH:/usr/local/go/bin' "/home/$USER/.profile"; then
+    echo "Adding go to /home/$USER/.profile becuse it is not there"
+    echo 'export PATH=$PATH:/usr/local/go/bin' >> /home/$USER/.profile
+fi
+source /home/$USER/.profile 
+go version
