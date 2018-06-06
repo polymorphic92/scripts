71638ac4c3eb3879f624f5b6849f0cc6c3834c81
Kyle Braham <polymorphic92@gmail.com>
2018-06-06 17:09:52 -0400

    pass in go version

--- install/golang-installer.sh
+++ install/golang-installer.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-VERSION='1.10.2'
+# VERSION='1.10.2'
 
-cd /home/$USER/Downloads && curl -O https://dl.google.com/go/go$VERSION.linux-amd64.tar.gz  #go.#.##.# needs to be a parmameter
+VERSION=$1
+
+cd /home/$USER/Downloads && curl -O https://dl.google.com/go/go$VERSION.linux-amd64.tar.gz
+
 sudo tar -C /usr/local -xzf go$VERSION.linux-amd64.tar.gz
-# sudo rm -f /usr/local/go$VERSION.linux-amd64.tar.gz
+sudo rm -f /usr/local/go$VERSION.linux-amd64.tar.gz
 sudo rm -f /home/$USER/Downloads/go$VERSION.linux-amd64.tar.gz
-
 
 if ! grep -q 'export PATH=$PATH:/usr/local/go/bin' "/home/$USER/.profile"; then
     echo "Adding go to /home/$USER/.profile becuse it is not there"
