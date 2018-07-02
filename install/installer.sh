781514df994da9141e066812a0f7268ae6ec9a4a
polymorphic92 <polymorphic92@gmail.com>
2018-07-02 08:40:00 -0400

    added vscode and vim-plug

--- install/installer.sh
+++ install/installer.sh
@@ -83,9 +83,21 @@ function postman {
     echo "You can use Postman!"
 
 }
+function vscode {
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+    sudo dnf install code
+    # https://code.visualstudio.com/docs/setup/linux
+}
 
-
-
+function vim-addons {
+    ##check vim is installed : 
+        #which vim
+    ##install vim-plug (manager): 
+        #curl -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    #install plugins:  
+        #vim +PlugInstall +qall
+}
 
 
 
