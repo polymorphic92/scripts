3bc0bbccdb813c48d20de54c9bc0c679e02ab965
polymorphic92 <polymorphic92@gmail.com>
2018-08-23 15:19:19 -0400

    updated script

--- start-minishift.sh
+++ start-minishift.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# need to check if minishift can be called
 
+minishift-version='v3.9.0'
 if type minishift >/dev/null 2>&1; then
-    minishift profile set tutorial
+    minishift profile set istio-tutorial
     minishift config set memory 4GB
     minishift config set cpus 2
     minishift config set vm-driver virtualbox
     minishift config set image-caching true
     minishift addon enable admin-user
-    minishift config set openshift-version v3.9.0
+    minishift config set openshift-version $minishift-version
+    minishift addon enable anyuid
     minishift start
 
     echo "using oc to log into minishift"
     eval $(minishift oc-env)
     eval $(minishift docker-env)
     oc login $(minishift ip):8443 -u admin -p admin
-
     minishift console
     oc get node 
 
