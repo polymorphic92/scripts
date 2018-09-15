d80d29ef1628c49aa86ef870906f70b517747b65
Kyle Braham <kbraham@localhost.localdomain>
2018-09-15 18:37:48 -0400

    reorigination of docker scritps into more generic dir devops

--- install/docker.sh
+++ install/docker.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Uninstall old versions
-sudo dnf remove docker \
+sudo dnf -y remove docker \
                 docker-client \
                 docker-client-latest \
                 docker-common \
@@ -13,32 +13,33 @@ sudo dnf remove docker \
                 docker-engine-selinux \
                 docker-engine
 
- # Install the dnf-plugins-core package - commands to manage your DNF repositories from the command line.
+
+# Install the dnf-plugins-core package - commands to manage your DNF repositories from the command line.
 sudo dnf -y install dnf-plugins-core
 
- # Set up the stable repository
-sudo dnf config-manager \
+# Set up the stable repository
+sudo dnf -y config-manager \
     --add-repo \
     https://download.docker.com/linux/fedora/docker-ce.repo
 
  # Install Docker CE
-sudo dnf install docker-ce
+sudo dnf -y install docker-ce
 
- # Start Docker
+#Start dokcer
 sudo systemctl start docker
 
- # Create the docker group
-sudo groupadd docker
+#Create the docker group
+if ! id -nG "$USER" | grep -qw "docker"; then
+    sudo usermod -aG docker $USER
+    printf "Added $USER to docker group\n\n" 
+else 
+    printf "already in docker group\n\n"
+fi
 
- # Add your user to the docker group
-sudo usermod -aG docker $USER
-
- # Enable docker to start on system boot via systemd
-
- sudo systemctl enable docker
+# Enable docker to start on system boot via systemd
+sudo systemctl enable docker
 
 # #install docker-compose 
-
 if ! [ -x "$(command -v docker-compose)" ]; then
   COMPOSE_VERSION=$(curl -s -o /dev/null -I -w "%{redirect_url}\n" https://github.com/docker/compose/releases/latest | grep -oP "[0-9]+(\.[0-9]+)+$")
   curl -L https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
@@ -46,5 +47,5 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   curl -L https://raw.githubusercontent.com/docker/compose/$COMPOSE_VERSION/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
   docker-compose --version
 else 
-    printf "docker-compose already installed"
+    printf "docker-compose already installed \n\n"
 fi
