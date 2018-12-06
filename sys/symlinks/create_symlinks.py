18db513be253e9c7fad619e1e9ce7b3e25a3eac5
polymorphic92 <polymorphic92@gmail.com>
2018-12-06 09:02:16 -0500

    updated output

--- sys/symlinks/create_symlinks.py
+++ sys/symlinks/create_symlinks.py
@@ -11,8 +11,9 @@ for link in links:
   full_src =  os.getcwd() + '/'  + link["src"]
   full_des =  os.path.expanduser('~') + '/' + link["des"]
 
+  print('Linking: ' + full_src + ' -> ' + full_des+':\t', end =" ")
   if os.path.exists(full_src) and not os.path.islink(full_des):
     if os.path.exists(full_des):
-      os.renames(full_des, home + '.baks/' + link["des"] + '.bak')  # rename file to ./baks/file.bak then delete it
-    print(full_src + ' -> ' + full_des)
+      os.renames(full_des, home + '.baks/' + link["des"] + '.bak')  # rename file to ./baks/file.bak
     os.symlink(full_src, full_des)
+    print('Success!!')
