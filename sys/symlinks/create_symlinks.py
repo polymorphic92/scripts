3cb5945342f3d9e47a811ea90ea40d804b3c72c4
polymorphic92 <polymorphic92@gmail.com>
2018-12-06 09:41:50 -0500

    broke up logic

--- sys/symlinks/create_symlinks.py
+++ sys/symlinks/create_symlinks.py
@@ -7,13 +7,22 @@ try:
 except Exception as ex:
   print(ex)
 
-for link in links:
-  full_src =  os.getcwd() + '/'  + link["src"]
-  full_des =  os.path.expanduser('~') + '/' + link["des"]
+home = os.path.expanduser('~') + '/' 
+cwd  = os.getcwd() + '/'
 
-  print('Linking: ' + full_src + ' -> ' + full_des+':\t', end =" ")
-  if os.path.exists(full_src) and not os.path.islink(full_des):
-    if os.path.exists(full_des):
-      os.renames(full_des, home + '.baks/' + link["des"] + '.bak')  # rename file to ./baks/file.bak
-    os.symlink(full_src, full_des)
-    print('Success!!')
+for link in links:
+  full_src = cwd  + link["src"]
+  full_des = home + link["des"]
+
+  if os.path.exists(full_src):
+    if not os.path.islink(full_des):
+      print('Linking: ' + full_src + ' -> ' + full_des +':\t')
+      if os.path.exists(full_des):
+        print("\tFile exists renaming")
+        os.renames(full_des, home + '.baks/' + link["des"] + '.bak')  # rename file to ./baks/file.bak
+      os.symlink(full_src, full_des)
+      print('\tSuccess!!')
+    else:
+      print('Already Linked:\t ' + full_src + ' -> ' + full_des)
+  else:
+    print(full_src + ": Does Not Exist")
