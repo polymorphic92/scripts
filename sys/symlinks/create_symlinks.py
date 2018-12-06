e68f3cf8a7b86397b8d8575ae5d4eb3aed620a47
polymorphic92 <polymorphic92@gmail.com>
2018-12-06 08:36:15 -0500

    clean up

--- sys/symlinks/create_symlinks.py
+++ sys/symlinks/create_symlinks.py
@@ -1,28 +1,18 @@
 #!/usr/bin/env python3
-
 import json, os 
 
 try:
   with open('config.json') as f:
-      data = json.load(f)
+      links = json.load(f)
 except Exception as ex:
   print(ex)
 
-home = os.path.expanduser('~') + '/'
-cwd = os.getcwd() + '/'
-
-for obj in data:
-  full_src =  cwd  + obj["src"]
-  full_des =  home + obj["des"]
+for link in links:
+  full_src =  os.getcwd() + '/'  + link["src"]
+  full_des =  os.path.expanduser('~') + '/' + link["des"]
 
   if os.path.exists(full_src) and not os.path.islink(full_des):
     if os.path.exists(full_des):
-      # rename file to file.bak then delete it
-      os.renames(full_des, home + 'bak/' + obj["des"] + '.bak')
-     # os.unlink(full_des)
+      os.renames(full_des, home + '.baks/' + link["des"] + '.bak')  # rename file to ./baks/file.bak then delete it
     print(full_src + ' -> ' + full_des)
-    os.symlink(full_src, full_des)  
-
-
-
-
+    os.symlink(full_src, full_des)
